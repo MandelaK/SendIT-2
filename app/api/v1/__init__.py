80a3e56dc31a2ca8cd71e675b9f1fdbf5349ba72
Cray <44587521+MandelaK@users.noreply.github.com>
2018-11-10 15:26:18 +0300

    Update __init__.py

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,4 +1,4 @@
-from .views import GenericParcel, User, SpecificParcel, Admin, Cancel
+from .views import GenericParcel, SpecificParcel
 from flask import Blueprint
 from flask_restful import Api
 
@@ -6,7 +6,7 @@ version1 = Blueprint('v1', __name__, url_prefix='/api/v1')
 api = Api(version1)
 
 api.add_resource(GenericParcel, '/parcels')
-api.add_resource(User, '/users/<string:id>/parcels')
+# api.add_resource(User, '/users/<string:id>/parcels')
 api.add_resource(SpecificParcel, '/parcels/<int:id>')
-api.add_resource(Admin, '/admin/location/<int:id>')
-api.add_resource(Cancel, '/parcels/<int:id>/cancel')
+# api.add_resource(Admin, '/admin/location/<int:id>')
+# api.add_resource(Cancel, '/parcels/<int:id>/cancel')
